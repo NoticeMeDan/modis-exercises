129758908c84b33f0baffb2b7187e9f08d034fa9
Elias Jørgensen <eliaszakarias@gmail.com>
2018-08-29 20:22:06 +0200

    Should listen for messages

--- echo/src/main/java/com/noticemedan/echo/Echo.java
+++ echo/src/main/java/com/noticemedan/echo/Echo.java
@@ -1,7 +1,27 @@
 package com.noticemedan.echo;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.StandardCharsets;
+
 public class Echo {
     public static void main(String[] args) {
-        System.out.println("Hi mom");
+        System.out.println("Starting server");
+        Echo.run();
+    }
+
+    private static void run() {
+        try {
+            DatagramChannel channel = DatagramChannel.open();
+            channel.connect(new InetSocketAddress("127.0.0.1", 7007));
+            ByteBuffer buffer = ByteBuffer.allocate(1000);
+            buffer.clear();
+            channel.receive(buffer);
+            System.out.println(String.format("Received: %s", new String(buffer.array(), StandardCharsets.UTF_8)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
